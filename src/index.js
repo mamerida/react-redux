e1a548c31e2cc9da4f1df7597c19afe1891bd4c2
Mario <mamerida2013@gmail.com>
2021-12-30 15:16:45 -0300

    cambio y trabajo con redux con primer ejemplo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux';
 import reportWebVitals from './reportWebVitals';
+
+//createStore es el encargado de recibir y despachar los estados. Los recibe en state y los despacha mediante actions
+//obligatorio retornar el estado 
+const store = createStore((state = 0,action) =>{ //reducer acion de createStore
+  //action = {type:'tipo de accion, payload: any}
+  //con la propiedad de payload puedo trabajarlo mediante un switch
+  switch (action.type){
+    case 'accion': {
+      return action.payload
+    }
+  }
+  return state
+})
+
+//store.getState() devuelve el estado completo del sistema 
+// console.log(store.getState())
+
+//store.dispatch({type:'accion'}) ejecutara el reducer de createStore osea volvera a ejecutar la accion 
+//con la propiedad de payload puedo acutalizar el estado
+
+store.dispatch({type:'accion' , payload: 2 })
+console.log(store.getState())
 
 ReactDOM.render(
   <React.StrictMode>
